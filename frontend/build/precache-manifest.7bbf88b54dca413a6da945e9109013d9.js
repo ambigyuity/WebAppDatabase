4b39a39171e3cfb88b84b052c58e28487dc4a706
ambigyuity <52841123+ambigyuity@users.noreply.github.com>
2020-12-21 16:22:41 -0600

    Add files via upload

---
+++ frontend/build/precache-manifest.7bbf88b54dca413a6da945e9109013d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8320d32403306b518bd34b12999508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b965bb39e57f4034dc4",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b0a73ae150c1c1665eb6",
+    "url": "/static/css/main.9a30924e.chunk.css"
+  },
+  {
+    "revision": "4b965bb39e57f4034dc4",
+    "url": "/static/js/2.76d40737.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.76d40737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a73ae150c1c1665eb6",
+    "url": "/static/js/main.43e87141.chunk.js"
+  },
+  {
+    "revision": "52bf146beb214a5db5c5",
+    "url": "/static/js/runtime-main.509426b1.js"
+  }
+]);
---
+++ frontend/build/precache-manifest.ebbb14f4887a5167ffa5650d605d0852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227614612c27c1575625ff146e3dc64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0778a7b408b7ff6ffeb6",
+    "url": "/static/js/2.aa8c6396.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.aa8c6396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c52331aab70168fea43",
+    "url": "/static/js/main.501260be.chunk.js"
+  },
+  {
+    "revision": "6b404c94ec1a97860a7a",
+    "url": "/static/js/runtime-main.a9f649d9.js"
+  }
+]);
